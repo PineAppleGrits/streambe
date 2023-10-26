5ec1c1746d100cc3805f9236a6980f2566b09c85
Gino S <66481194+PineAppleGrits@users.noreply.github.com>
2023-10-26 11:01:19 -0300

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import { transformDocument } from "@prisma/client/runtime";
 import { NextResponse } from "next/server";
 export async function GET() {
+  return NextResponse.json([{id:1,email:"admin@admin.com"}]);
   const res = await prisma.user.findMany({
     select: {
       id: true,
